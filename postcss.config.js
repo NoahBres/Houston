d1f9b5f0138548298a062fc181e1bde4bd3ff765
Noah <noahbres@gmail.com>
2019-05-10 05:08:07 -0500

    Fixed PurgeCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,17 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: [
-    "./src/**/*.html",
-    "./src/**/*.vue",
-    "./src/**/*.jsx"
-    // etc.
-  ],
+// const purgecss = require("@fullhuman/postcss-purgecss")({
+//   // Specify the paths to all of the template files in your project
+//   content: [
+//     "./public/**/*.html",
+//     "./src/**/*.js"
+//     // etc.
+//   ],
 
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+//   // Include any special characters you're using in this regular expression
+//   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+// })
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["**/*.html", "**/*.js"]
 });
 
 module.exports = {
