67ea9b49a12f7dceea44b0cedf8965145a4560ea
Noah <noahbres@gmail.com>
2019-05-10 05:09:20 -0500

    Implement Sidebar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React from "react";
-import { Route, NavLink, HashRouter } from "react-router-dom";
+import { Route, HashRouter } from "react-router-dom";
 
 import Dashboard from "./views/Dashboard";
 import Logs from "./views/Logs";
 import Settings from "./views/Settings";
 
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 export default function App() {
   return (
     <HashRouter>
-      <Navbar />
-      <ul className="list-disc pl-6">
-        <li>
-          <NavLink to="/">Dashboard</NavLink>
-        </li>
-        <li>
-          <NavLink to="/logs">Logs</NavLink>
-        </li>
-        <li>
-          <NavLink to="/settings">Home</NavLink>
-        </li>
-      </ul>
-      <div className="content">
-        <Route exact path="/" component={Dashboard} />
-        <Route path="/logs" component={Logs} />
-        <Route path="/settings" component={Settings} />
+      <Navbar title="Houston" />
+      <div className="flex flex-row" style={{ height: "calc(100vh - 75px)" }}>
+        <Sidebar title="Team 10940" avatar="react-logo.png" />
+        <section className="content">
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/logs" component={Logs} />
+          <Route path="/settings" component={Settings} />
+        </section>
       </div>
     </HashRouter>
   );
