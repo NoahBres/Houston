1c639b57bebac3f141f790895e66bacadd16ad8d
Noah <noahbres@gmail.com>
2019-05-10 02:43:20 -0500

    Remove custom file specification

--- postcss.config.js
+++ postcss.config.js
@@ -13,7 +13,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 
 module.exports = {
   plugins: [
-    require("tailwindcss")("./tailwind.config.js"),
+    require("tailwindcss"),
     require("autoprefixer"),
     ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
