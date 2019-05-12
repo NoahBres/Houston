35a55e66fbc1b39c5308aafa53050f9641a4a0ac
Noah <noahbres@gmail.com>
2019-05-12 12:42:56 -0500

    Use routes.js and switch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React from "react";
-import { Route, HashRouter } from "react-router-dom";
+import { Route, HashRouter, Switch } from "react-router-dom";
 
-import Dashboard from "./views/Dashboard";
-import Logs from "./views/Logs";
-import Settings from "./views/Settings";
+import routes from "./routes";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -14,10 +11,19 @@ export default function App() {
       <Navbar title="Houston" />
       <div className="flex flex-row" style={{ height: "calc(100vh - 75px)" }}>
         <Sidebar title="Team 10940" avatar="react-logo.png" />
-        <section className="content">
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/logs" component={Logs} />
-          <Route path="/settings" component={Settings} />
+        <section className="content w-full pl-2 pr-5">
+          <Switch>
+            {routes.map((prop, key) => {
+              return (
+                <Route
+                  path={prop.path}
+                  component={prop.component}
+                  exact={prop.exact}
+                  key={key}
+                />
+              );
+            })}
+          </Switch>
         </section>
       </div>
     </HashRouter>
