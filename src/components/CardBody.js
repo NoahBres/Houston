eecd2c6eed356f0abcdb3681ce4b70f2464f6ade
Noah <noahbres@gmail.com>
2019-05-12 14:36:35 -0500

    Add CardBody and CardHeader components

---
+++ src/components/CardBody.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CardBody(props) {
+  return <div className="">{props.children}</div>;
+}
---
+++ src/components/CardHeader.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CardHeader(props) {
+  return <div className="px-4 pt-4">{props.children}</div>;
+}
