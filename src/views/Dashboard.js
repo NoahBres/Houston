215050d62f0e29ae1441808aa6f7edadbe2dd71e
Noah <noahbres@gmail.com>
2019-05-12 14:37:25 -0500

    Add LogCard to dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import Card from "../components/Card";
+import LogCard from "../components/LogCard";
 
 export default function Dashboard() {
   return (
     <main className="">
       <div className="flex flex-row">
-        <Card className="h-56" width="1" />
+        <LogCard />
       </div>
     </main>
   );
