ad57df5e50f322fd06004d3686fd3a633cd396d1
Noah <noahbres@gmail.com>
2019-05-13 02:04:02 -0500

    Implement auto-scorlling in LogCard

--- src/components/LogCard.js
+++ src/components/LogCard.js
@@ -1,26 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
+import useStayScrolled from "react-stay-scrolled";
 
 import Card from "./Card";
-import CardHeader from "./CardHeader";
-import CardBody from "./CardBody";
 
 import SocketClient from "../SocketClient";
 
 export default function LogCard(props) {
   const [logs, setLogs] = useState([]);
 
+  const listRef = useRef(null);
+  const { stayScrolled } = useStayScrolled(listRef);
+
   const messageListener = msg => {
-    // setLogs(m => m.concat(msg));
-    setLogs(l => l.concat(msg, msg, msg));
+    setLogs(l => l.concat(msg));
   };
 
   useEffect(() => {
     SocketClient.addMessageListener(messageListener);
 
+    setInterval(() => {
+      messageListener(`test`);
+    }, 2000);
+
     return function cleanup() {
       SocketClient.removeMessageListener(messageListener);
     };
   }, []);
+
+  useLayoutEffect(() => {
+    stayScrolled();
+  }, [logs]);
 
   return (
     <Card className={props.className} height={props.height}>
@@ -28,10 +37,12 @@ export default function LogCard(props) {
         <h5 className="text-xs font-light text-gray-600">Real time</h5>
         <h2 className="text-3xl font-thin">Logging</h2>
       </div>
-      <div className="p-4">
-        <ul>
+      <div className="px-4 my-3">
+        <ul ref={listRef} className="overflow-auto" style={{ height: "20rem" }}>
           {logs.map((e, i) => (
-            <li key={i}>{e}</li>
+            <li key={i}>
+              {i} {e}
+            </li>
           ))}
         </ul>
       </div>
