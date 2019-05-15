45530093a79aca506549b60e5c621752c604f2a9
Noah <noahbres@gmail.com>
2019-05-14 23:09:04 -0500

    Inject width and height in style

--- src/components/SvgIcon.js
+++ src/components/SvgIcon.js
@@ -1,31 +1,27 @@
 import React from "react";
 
 export default function SvgIcon({
-  style={},
-  fill="#000",
-  width="100%",
-  className="",
+  style = {},
+  fill = "#000",
+  width = "100%",
+  className = "",
   // viewBox="0 0 32 32",
-  viewBox=[32, 32],
-  d=""
+  viewBox = [32, 32],
+  d = ""
 }) {
-  return(
+  return (
     <svg
       width={width}
       height={width}
-      style={style}
+      style={{ ...style, height: width, width: width }}
       viewBox={`0 0 ${viewBox[0]} ${viewBox[1]}`}
       xmlns="http://www.w3.org/2000/svg"
       className={`${className}`}
       xmlnsXlink="http://www.w3.org/1999/xlink"
     >
-      {
-        d.map((path, key) => {
-          return (
-            <path fill={fill} d={path} key={key}/>
-          )
-        })
-      }
+      {d.map((path, key) => {
+        return <path fill={fill} d={path} key={key} />;
+      })}
     </svg>
-  )
+  );
 }
