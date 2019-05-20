fb807dd6e663c9c98c3cd90fd24b8a5c999b2f91
Noah <noahbres@gmail.com>
2019-05-20 03:36:16 -0500

    CommandCard progress. Remote Logging Button and Command Input

--- src/components/CommandCard.js
+++ src/components/CommandCard.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "./Card";
 
 import SocketClient from "../SocketClient";
 
 export default function CommandCard({ className = "" }) {
+  const [isLogging, setIsLogging] = useState(false);
+
+  function handleRemoteLoggingClick() {
+    setIsLogging(i => !i);
+  }
+
   return (
     <Card className={`${className}`}>
       <div className="px-4 pt-4">
@@ -12,9 +18,28 @@ export default function CommandCard({ className = "" }) {
         <h2 className="text-3xl font-thin">Commands</h2>
       </div>
       <div className="px-4 my-3">
-        <div className="flex flex-row self-end">
-          <input className=" bg-notblack-lighter leading-tight text-gray-500 appearance-none rounded-full w-full text-gray-700 px-3 py-2 focus:outline-none focus:bg-white border-2 border-notblack-lighter focus:border-purple-500 transition-300-ease" type="text" placeholder="Type your command"/>
-          <button className="px-3 py-2">Send</button>
+        <div className="flex flex-row py-2 items-center">
+          <label className="font-light tracking-wide">Remote Logging: </label>
+          <button
+            className={`ml-3 text-black uppercase tracking-wide font-medium text-sm ${
+              isLogging
+                ? "bg-red-700 hover:bg-red-600 text-white"
+                : "bg-green-500 hover:bg-green-700"
+            } px-3 py-1 rounded transition-300-ease`}
+            onClick={handleRemoteLoggingClick}
+          >
+            {isLogging ? "Stop" : "Start"}
+          </button>
+        </div>
+        <div className="flex flex-row py-3 pt-6">
+          <input
+            className="bg-notblack-lighter leading-tight text-gray-500 appearance-none rounded-full w-full text-gray-700 px-4 py-2 focus:outline-none focus:bg-white border-2 border-notblack-lighter focus:border-purple-500 transition-300-ease"
+            type="text"
+            placeholder="Type your command"
+          />
+          <button className="px-3 py-2 hover:text-gray-500 transition-300-ease">
+            Send
+          </button>
         </div>
       </div>
     </Card>
