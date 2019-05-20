addb215a9eddee96f331b4f3a91dc4d1bd572908
Noah <noahbres@gmail.com>
2019-05-20 04:10:18 -0500

    Fix ESLint issues for Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Card({ className = "", children = [], height = "" }) {
+  return (
+    <div
+      className={`${className} bg-notblack shadow-lg rounded mx-3`}
+      style={{ height }}
+    >
+      {children}
+    </div>
+  );
+}
+
+Card.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.element,
+  height: PropTypes.string
+};
+
+Card.defaultProps = {
+  className: "",
+  children: [],
+  height: ""
+};
+
+export default Card;
