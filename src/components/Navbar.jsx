169ff7e0c090023da635f4eb00d4d6ab9fd95666
Noah <noahbres@gmail.com>
2019-05-23 22:30:44 -0500

    Update Navbar to comply with ESLint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Navbar(props) {
+export default function Navbar({ title = "" }) {
   return (
     <nav className="w-full px-6 py-5">
       <h1 className="text-lg uppercase font-light tracking-wide py-1 mx-4 inline-block">
-        {props.title}
+        {title}
       </h1>
     </nav>
   );
 }
+
+Navbar.propTypes = {
+  title: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: ""
+};
