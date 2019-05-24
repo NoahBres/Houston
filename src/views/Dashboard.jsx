ce9db5fa97e842c22a6747938aa0ddef6122034c
Noah <noahbres@gmail.com>
2019-05-23 23:40:14 -0500

    Update Dashboard to comply with ESLint

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import LogCard from "../components/LogCard";
 import SocketInfoCard from "../components/SocketInfoCard";
